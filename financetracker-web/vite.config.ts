76f78721a4358ee32768a86914706e16d7b08f40
dreyssechris <chrisdreysse@gmail.com>
2025-10-20 18:07:02 +0200

    vite.config.ts now contains base '/financetracker/' | Caddyfile adjusted accordingly

--- financetracker-web/vite.config.ts
+++ financetracker-web/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/financetracker/',   // !important: Dev & Prod same sub-path
   server: {
     port: 5173,
-  }
+    // should be enough HMR goes through Caddy reverse proxy
+    // If HMR has issues behind proxy, set IP address hard:
+    // origin: 'http://192.168.0.168:8080',
+    // hmr: { protocol: 'ws', host: '192.168.0.168', port: 8080 },
+  },
 })
