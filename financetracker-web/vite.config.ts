7507fdf7a513296c4fbc7e5659941c88dd221ef2
dreyssechris <chrisdreysse@gmail.com>
2025-10-20 21:04:33 +0200

    ...

--- financetracker-web/vite.config.ts
+++ financetracker-web/vite.config.ts
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/financetracker/',   // !important: Dev & Prod same sub-path
+  base: '/financetracker',   // !important: Dev & Prod same sub-path
   server: {
     port: 5173,
-    host: true,
-    allowedHosts: true,
     // should be enough HMR goes through Caddy reverse proxy
     // If HMR has issues behind proxy, set IP address hard:
     // origin: 'http://192.168.0.168:8080',
