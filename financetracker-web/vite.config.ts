3b52490dd91b830acac25374e03c827092ce28f3
dreyssechris <chrisdreysse@gmail.com>
2025-10-20 18:34:17 +0200

    changes reversed

--- financetracker-web/vite.config.ts
+++ financetracker-web/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     // should be enough HMR goes through Caddy reverse proxy
     // If HMR has issues behind proxy, set IP address hard:
-    origin: 'http://192.168.0.168:8080',
-    hmr: { protocol: 'ws', host: '192.168.0.168', port: 8080 },
+    // origin: 'http://192.168.0.168:8080',
+    // hmr: { protocol: 'ws', host: '192.168.0.168', port: 8080 },
   },
 })
